fb005e7b24bfb72ced26915673369e1e22e5f741
Fabrizio Nazar <fabriziometal63@gmail.com>
2022-04-04 11:17:30 -0500

    First commint
    Traffic generator for a website using Selenium, for desktop and mobile view.

---
+++ PageViewsGenerator/desktop.py
@@ -0,0 +1,71 @@
+import random
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from random import randrange
+
+#import warnings
+#warnings.filterwarnings("ignore")
+
+if __name__ == '__main__':
+
+    def scroll_to_bottom():
+        scroll = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        scroll.maximize_window()
+        time.sleep(random.randint(2, 5))
+        scroll.get('https://www.shopkarenberrios.com')
+        time.sleep(random.randint(10,25))
+        scroll.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(random.randint(3,6))
+        scroll.close()
+        print('DONE')
+
+    def google_traffic():
+        #CLASS FROM INPUT FROM GOOGLE.COM
+        searchBarInputClass = 'q'
+        #CLASS FROM SEARCH BUTTON
+        btnClass = 'btnK'
+        #LINK FROM WEBSITE TO SEARCH
+        link = 'https://shopkarenberrios.com/collections/superfoods'
+        scroll = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        #scroll = webdriver.Chrome("C:\\DRIVER\\chromedriver.exe")
+        scroll.maximize_window()
+        time.sleep(random.randint(1, 3))
+        scroll.get('https://www.google.com')
+        time.sleep(random.randint(2,3))
+        scroll.find_element_by_name(searchBarInputClass).send_keys('Shop Karen Berrios')
+        time.sleep(random.randint(2, 3))
+        scroll.find_element_by_name(btnClass).click()
+        time.sleep(random.randint(1, 3))
+        scroll.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(random.randint(4, 6))
+        scroll.close()
+        print('DONE')
+
+    def click_on_link():
+        link_list = ['BLOG', 'BUNDLES', 'SUPERFOODS']
+        #link_list = ['RECIPES', 'SUPERFOODS']
+        i = randrange(len(link_list))
+        link = link_list[i]
+        scroll = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        #scroll = webdriver.Chrome("C:\\DRIVER\\chromedriver.exe")
+        scroll.maximize_window()
+        print('MAXIMIZA VENTANA')
+        time.sleep(random.randint(1, 3))
+        print('CARGA PÁGINA')
+        scroll.get('https://www.shopkarenberrios.com')
+        print('CLIC EN LINK')
+        time.sleep(random.randint(5, 10))
+        scroll.find_element_by_partial_link_text(link).click()
+        print('ESPERA')
+        time.sleep(random.randint(10,100))
+        scroll.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(random.randint(1,3))
+        scroll.close()
+        print('DONE')
+
+#LIST OF FUNCTION
+function_list = [click_on_link, scroll_to_bottom,google_traffic]
+#CALLS RANDOM FUNCTION FROM LIST
+random.choice(function_list)()
